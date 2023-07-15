6d500f04dc1966caee2735ff73f420d573beca29
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-15 15:42:47 -0400

    implementing features of log in

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,14 @@ import "./login.scss"
 
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
