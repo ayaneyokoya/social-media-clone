ab41b7facf10e34c9ec3b9d84bdb56842d1003e1
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-16 16:06:33 -0400

    implemented router feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  return <div><Register/></div>;
+
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login/>
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ]);
+
+  return <div> <RouterProvider router={router} /> </div>;
 }
 
 export default App;
