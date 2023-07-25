1e92e4e2d1ca14120766d10c2e090f19b5868e51
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-25 12:44:07 -0400

    imported material UI icons

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,10 @@ function App() {
 
   const ProtectedRoute = ({children}) => {
     if (!currentUser) {
-      return <Navigate to="/login" />
+      return <Navigate to="/login" />;
     }
-    return children
-  }
+    return children;
+  };
 
   const router = createBrowserRouter([
     {
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,4 +1,13 @@
 import "./navBar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
 
 const NavBar = () => {
   return (
