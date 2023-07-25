105e119bf471eca5e56be7c7486bfa53b8e53c84
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-25 14:31:39 -0400

    added left components

--- src/components/leftbar/LeftBar.jsx
+++ src/components/leftbar/LeftBar.jsx
@@ -67,6 +67,22 @@ const LeftBar = () => {
                     <span>Messages</span>
                 </div>
             </div>
+            <hr />
+            <div className="menu">
+                <span>Others</span>
+                <div className="item">
+                    <img src={Fund} alt="" />
+                    <span>Fundraiser</span>
+                </div>
+                <div className="item">
+                    <img src={Tutorials} alt="" />
+                    <span>Tutorials</span>
+                </div>
+                <div className="item">
+                    <img src={Courses} alt="" />
+                    <span>Courses</span>
+                </div>
+            </div>
         </div>
     </div>
   )
