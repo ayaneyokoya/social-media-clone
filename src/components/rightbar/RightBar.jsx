3b83a4808e03e856faafe658e17ebb77c676240c
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-25 15:02:29 -0400

    added latest activity feed

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -27,6 +27,36 @@ const RightBar = () => {
                     </div>
                 </div>
             </div>
+            <div className="item">
+                <span>Latest Activities</span>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="/src/assets/pfp.jpeg" alt="" />
+                        <p>
+                            <span>Jane Doe</span> changed their cover picture.
+                        </p>
+                    </div>
+                    <span>1 min ago</span>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="/src/assets/pfp.jpeg" alt="" />
+                        <p>
+                            <span>Jane Doe</span> commented on your post.
+                        </p>
+                    </div>
+                    <span>3 mins ago</span>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="/src/assets/pfp.jpeg" alt="" />
+                        <p>
+                            <span>Jane Doe</span> just posted a story.
+                        </p>
+                    </div>
+                    <span>10 mins ago</span>
+                </div>
+            </div>
         </div>
     </div>
   )
