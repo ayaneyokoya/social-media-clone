e62918a95180a7b73bab6f8f3336301aa53ac6bd
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-25 12:36:28 -0400

    implemented a method to check if user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import LeftBar from "./components/leftbar/LeftBar";
@@ -13,6 +14,8 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
 function App() {
+
+  const currentUser = false;
 
   const Layout = () => {
     return(
@@ -24,13 +27,24 @@ function App() {
           <RightBar />
         </div>
       </div>
-    )
+    );
+  };
+
+  const ProtectedRoute = ({children}) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+    return children
   }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout/>
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
