a9c17ece8f027abdf3555279a6c3b35edabfbfe5
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-25 12:31:14 -0400

    js -> jsx

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import LeftBar from "./components/leftbar/LeftBar";
+import RightBar from "./components/rightbar/RightBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import LeftBar from "./components/leftbar/LeftBar";
+import RightBar from "./components/rightbar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+
+  const Layout = () => {
+    return(
+      <div>
+        <NavBar />
+        <div style={{ display:"flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login/>
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ]);
+
+  return <div> <RouterProvider router={router} /> </div>;
+}
+
+export default App;
