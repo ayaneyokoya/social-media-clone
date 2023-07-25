a74848b75496336e437f2ac8b409bd13bcad6969
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-25 14:55:13 -0400

    added right bar components

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -2,7 +2,33 @@ import "./rightBar.scss"
 
 const RightBar = () => {
   return (
-    <div className="rightBar">RightBar</div>
+    <div className="rightBar">
+        <div className="container">
+            <div className="item">
+                <span>Suggested</span>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="/src/assets/pfp.jpeg" alt="" />
+                        <span>Jane Doe</span>
+                    </div>
+                    <div className="buttons">
+                        <button>Follow</button>
+                        <button>Dismiss</button>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src="/src/assets/pfp.jpeg" alt="" />
+                        <span>Jane Doe</span>
+                    </div>
+                    <div className="buttons">
+                        <button>Follow</button>
+                        <button>Dismiss</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
